5434ad9ba570d72d64104e6c94b39df21f4d6819
Seoyeon Choi <lala1470@konkuk.ac.kr>
2024-03-08 22:59:19 +0900

    Project Setting

---
+++ watchOS-Practice Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchOS-Practice Watch App
+//
+//  Created by Seoyeon Choi on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchOS-Practice Watch App/watchOS_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOS_PracticeApp.swift
+//  watchOS-Practice Watch App
+//
+//  Created by Seoyeon Choi on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Practice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
